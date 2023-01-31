4997326a1b60aa7c81fe387d2661f7da68b33e62
Ahmed Shaban <ahmed.shaaban.elhdah@gmail.com>
2023-01-31 13:39:11 +0200

    add executor and main executor
    - use executor
    - add ui effect - event - state

---
+++ shared/src/commonMain/kotlin/com/example/km_movie_app/presentation/core/executor/IExecutorScope.kt
@@ -0,0 +1,9 @@
+package com.example.km_movie_app.presentation.core.executor
+
+/**
+ * get it from the internet
+ * Created by Daniel Ávila Domingo on 6/10/21.
+ **/
+interface IExecutorScope {
+    fun cancel()
+}
